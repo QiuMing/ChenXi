e6d5ab1241b72d41dc6c0fadd8e12bbe026c3da7
sunshine <932762456@qq.com>
2016-02-16 19:05:49 +0800

    LoginActivity and RegisterActivity UI

---
+++ app/src/main/java/com/hunter/chenxi/ui/activity/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.hunter.chenxi.ui.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hunter.chenxi.R;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+}
