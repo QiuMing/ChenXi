ec9a53361c7d6331380773f6a469741df30d94a7
sunshine <932762456@qq.com>
2016-02-16 21:27:06 +0800

    small change

--- app/src/main/java/com/hunter/chenxi/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/hunter/chenxi/ui/activity/RegisterActivity.java
@@ -17,7 +17,7 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+//sss
     }
 
     @Override
